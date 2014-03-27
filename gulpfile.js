d77c54f81dba4955e205bf3f8ef9a0ebc27b1499
Justin Chmura <justin.chmura@gmail.com>
2014-03-27 06:26:24 -0700

    Removed extra files that were a little bit overkill for this plugin.

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var jshint = require('gulp-jshint');
-
-gulp.task('lint', function () {
-  gulp.src(['jquery.bootstrap-chooser.js', 'gulpfile.js', 'tests/**/*.js'])
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
-});
