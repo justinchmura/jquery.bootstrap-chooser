5ef2faf9b9a03dbf50ef7c6476cd92dc93fd1d0b
Justin Chmura <justin.chmura@gmail.com>
2014-05-12 08:13:34 -0700

    Made the plugin clear linting errors.

--- jquery.bootstrap-chooser.js
+++ jquery.bootstrap-chooser.js
@@ -1,4 +1,6 @@
 (function ($) {
+  'use strict';
+
   $.chooser = function (el, options) {
 
     // -----------------------------------------------------------------------------------------------------------
@@ -8,7 +10,7 @@
     var base = this;
     base.$el = $(el);
     base.el = el;
-    base.$el.data("chooser", base);
+    base.$el.data('chooser', base);
     base.selected = [];
     base.public = {};
 
@@ -19,7 +21,7 @@
     base.init = function () {
       base.options = $.extend({}, $.chooser.defaultOptions, options);
 
-      base.$el.addClass("bootstrap-chooser");
+      base.$el.addClass('bootstrap-chooser');
 
       if ($.isArray(base.options.data)) {
         base.data = base.options.data;
@@ -32,19 +34,19 @@
       base._createSearchField();
       base._createDropdown();
 
-      base.$control.on("click.bootstrap-chooser", function () {
-        base.$control.addClass("active");
+      base.$control.on('click.bootstrap-chooser', function () {
+        base.$control.addClass('active');
         base.$input.focus();
-        base.$dropdown.addClass("active");
+        base.$dropdown.addClass('active');
       });
-      base.$dropdown.on("click.bootstrap-chooser", "li.option", function () {
+      base.$dropdown.on('click.bootstrap-chooser', 'li.option', function () {
         base._addSelection($(this));
         base._closeDropdown();
       });
-      base.$control.on("click.bootstrap-chooser", ".choice button.close", function (e) {
-        var index = $(this).data("index");
-        console.log($(this).data("index"));
-        for (var i = base.selected.length - 1; i >= 0; i--) {
+      base.$control.on('click.bootstrap-chooser', '.choice button.close', function (e) {
+        var index = $(this).data('index');
+        console.log($(this).data('index'));
+        for (var i = base.selected.length - 1; i >= 0; i -= 1) {
           if (base.selected[i].index === index) {
             base.selected.splice(i, 1);
           }
@@ -53,11 +55,11 @@
         e.preventDefault();
         return false;
       });
-      base.$control.on("input", "li.search-field input", function () {
+      base.$control.on('input', 'li.search-field input', function () {
         var search = $(this).val();
         base._search(search);
       });
-      $(document).on("click.bootstrap-chooser", function (e) {
+      $(document).on('click.bootstrap-chooser', function (e) {
         if (!base.$el.is($(e.target).closest('.bootstrap-chooser'))) {
           base._closeDropdown();
         }
@@ -77,7 +79,7 @@
         return;
       }
       if (base.$input.val().length > 0) {
-        base.$input.val("");
+        base.$input.val('');
       }
       base.unfiltered = base.data.slice();
       base._closeDropdown();
@@ -90,14 +92,14 @@
       base._closeDropdown();
       base.selected = [];
       base._applySelected();
-      base.$control.off("click.bootstrap-chooser");
-      base.$dropdown.off("click.bootstrap-chooser", "li.choice");
-      base.$control.off("click.bootstrap-chooser", ".choice button.close");
-      base.$control.removeClass("form-control");
-      base.$el.removeClass("chooser");
-      base.$el.removeData("plugins.bootstrap-chooser");
-      base.$el.html("");
-      $(document).off("click.bootstrap-chooser");
+      base.$control.off('click.bootstrap-chooser');
+      base.$dropdown.off('click.bootstrap-chooser', 'li.choice');
+      base.$control.off('click.bootstrap-chooser', '.choice button.close');
+      base.$control.removeClass('form-control');
+      base.$el.removeClass('chooser');
+      base.$el.removeData('plugins.bootstrap-chooser');
+      base.$el.html('');
+      $(document).off('click.bootstrap-chooser');
     };
 
     base.public.selections = function () {
@@ -109,25 +111,25 @@
     // -----------------------------------------------------------------------------------------------------------
 
     base._applySelected = function () {
-      $("li.choice", base.$control).remove();
+      $('li.choice', base.$control).remove();
       base.selected.forEach(function (item) {
-        var $close = $("<button/>").attr({
-          "type": "button",
-          "class": "close",
-          "aria-hidden": "true"
-        }).data("index", item.index).html("&times;");
-        var $label = $("<span/>").addClass("label label-" + base.options.labelClass).text(item.text);
-        var $item = $("<li/>").addClass("choice").data("index", item.index);
+        var $close = $('<button/>').attr({
+          'type': 'button',
+          'class': 'close',
+          'aria-hidden': 'true'
+        }).data('index', item.index).html('&times;');
+        var $label = $('<span/>').addClass('label label-' + base.options.labelClass).text(item.text);
+        var $item = $('<li/>').addClass('choice').data('index', item.index);
         $close.appendTo($label);
         $label.appendTo($item);
-        $item.insertBefore($("li.search-field", base.$control));
+        $item.insertBefore($('li.search-field', base.$control));
       });
-      $("li", base.$dropdown).removeClass("chosen").addClass("option").each(function () {
-        var index = $(this).data("index");
+      $('li', base.$dropdown).removeClass('chosen').addClass('option').each(function () {
+        var index = $(this).data('index');
         var $item = $(this);
         base.selected.forEach(function (item) {
           if (item.index === index) {
-            $item.removeClass("option").addClass("chosen");
+            $item.removeClass('option').addClass('chosen');
           }
         });
       });
@@ -145,7 +147,7 @@
               text: base.unfiltered[index]
             });
             if (base.$input.val().length > 0) {
-              base.$input.val("");
+              base.$input.val('');
               base.data = base.unfiltered.slice();
               base._resetDropdown();
             }
@@ -159,12 +161,12 @@
     };
 
     base._createSearchField = function () {
-      base.$el.html("<div class=\"bootstrap-chooser-control form-control\"><ul><li class=\"search-field\"></li></ul></div>");
-      $("<input/>").attr({
-        type: "search",
+      base.$el.html('<div class=\'bootstrap-chooser-control form-control\'><ul><li class=\'search-field\'></li></ul></div>');
+      $('<input/>').attr({
+        type: 'search',
         placeholder: base.options.placeholder,
-        autocomplete: "off"
-      }).appendTo($("li.search-field", base.$el));
+        autocomplete: 'off'
+      }).appendTo($('li.search-field', base.$el));
     };
 
     base._search = function (search) {
@@ -178,43 +180,43 @@
         base.data = base.unfiltered.slice();
       }
       base._resetDropdown();
-      $("li", base.$dropdown).each(function () {
+      $('li', base.$dropdown).each(function () {
         var item = $(this).text();
         if (base.selected.map(function (x) {
           return x.text;
         }).indexOf(item) !== -1) {
-          $(this).removeClass("option").addClass("chosen");
+          $(this).removeClass('option').addClass('chosen');
         }
       });
     };
 
     base._createDropdown = function () {
-      base.$control = $(".bootstrap-chooser-control", base.$el);
-      base.$input = $("li.search-field input", base.$control);
-      $("<div/>").html("<ul></ul>").addClass("bootstrap-chooser-dropdown").insertAfter(base.$control);
-      base.$dropdown = $(".bootstrap-chooser-dropdown", base.$el);
+      base.$control = $('.bootstrap-chooser-control', base.$el);
+      base.$input = $('li.search-field input', base.$control);
+      $('<div/>').html('<ul></ul>').addClass('bootstrap-chooser-dropdown').insertAfter(base.$control);
+      base.$dropdown = $('.bootstrap-chooser-dropdown', base.$el);
       base.$dropdown.hide();
       base._resetDropdown();
     };
 
     base._resetDropdown = function () {
-      $("ul", base.$dropdown).html("");
+      $('ul', base.$dropdown).html('');
       base.data.forEach(function (option, i) {
-        $("<li/>").text(option).data("index", i).addClass("option").appendTo($("ul", base.$dropdown));
+        $('<li/>').text(option).data('index', i).addClass('option').appendTo($('ul', base.$dropdown));
       });
     };
 
     base._closeDropdown = function () {
-      base.$control.removeClass("active");
-      base.$dropdown.removeClass("active");
+      base.$control.removeClass('active');
+      base.$dropdown.removeClass('active');
     };
 
     base.init();
   };
 
   $.chooser.defaultOptions = {
-    placeholder: "Choose...",
-    labelClass: "info",
+    placeholder: 'Choose...',
+    labelClass: 'info',
     data: [],
     max: null,
     select: null
@@ -224,16 +226,16 @@
     var args = Array.prototype.slice.call(arguments, 0);
     return this.each(function () {
       var $this = $(this);
-      var data = $this.data("plugins.bootstrap-chooser");
-      var options = typeof option === "object" && option;
-      if (!data && option === "destroy") {
+      var data = $this.data('plugins.bootstrap-chooser');
+      var options = typeof option === 'object' && option;
+      if (!data && option === 'destroy') {
         return;
       } else if (!data) {
-        $this.data("plugins.bootstrap-chooser", new $.chooser(this, options));
-      } else if (typeof option === "string") {
+        $this.data('plugins.bootstrap-chooser', new $.chooser(this, options));
+      } else if (typeof option === 'string') {
         args.splice(0, 1);
         return data.public[option].apply(data, args);
       }
     });
   };
-})(jQuery);
+})(window.jQuery);
